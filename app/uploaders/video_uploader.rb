17d01b0e3d5bd49c00301a9bf8b8163d6f1a9b43
Kurnia Muhamad <omenkzz@gmail.com>
2020-06-04 03:10:05 +0700

    Upload video to cloudinary

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class VideoUploader < CarrierWave::Uploader::Base
+  include Cloudinary::CarrierWave
   include CarrierWave::MiniMagick
   include CarrierWave::Video
   include CarrierWave::Video::Thumbnailer
